6334d834980ef0fcd0bf80a5744ad8e96c999c90
Martin Davis <martin_davis3@yahoo.ca>
2020-03-06 10:50:07 -0700

    First examples

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+/**
+ * Let's try Jacva scropt afgain!
+ * Multi-line comment
+ */
+
+// Single line comment
+
+// Basic assignment:
+var myVariable='38.987'
+console.log(  myVariable + 5);  //Ooop, one of these is a string so plus is concatenation
+
+myVariable=parseFloat( myVariable); //Floating point number, decimal maintained
+console.log( myVariable );
+
+myVariable = parseInt ( myVariable);
+console.log( myVariable )
+
+myVariable = Number( myVariable );     //Converted to any number type it can.
+console.log( myVariable)
+
+myVariable = myVariable + 5;  //Addition
+console.log( myVariable )
+
+myVariable = myVariable.toString();
+console.log( myVariable )
+
+/**
+ *  Other types?
+ */
+
+ 
